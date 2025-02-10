48f288c9aa76bb9df2b10175063db3a4c95b54e8
전병길 <jeonbg@kakao.com>
2025-02-10 21:06:35 +0900

    ✨ feat: 블로그 포스트 빈 이미지 스타일 추가

--- src/components/blog/blog-item.tsx
+++ src/components/blog/blog-item.tsx
@@ -21,7 +21,8 @@ export function BlogItem({ page }: { page: InferPageType<typeof blog> }) {
             sizes="(max-width: 760px) 90vw, 400px"
           />
         ) : (
-          <div className="relative flex h-full items-center justify-center font-mono text-muted-foreground">
+          <div className="relative flex h-full items-center justify-center font-mono text-4xl text-foreground">
+            <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] bg-[size:20px_20px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_80%,transparent_100%)]" />
             Afaik
           </div>
         )}
