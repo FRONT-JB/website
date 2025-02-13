4c0bbe4a12e445eee2b659d6bbe8799f3116a344
전병길 <jeonbg@kakao.com>
2025-02-13 23:50:49 +0900

    🔧 config: docs, meta 데이터 설정 수정

--- source.config.ts
+++ source.config.ts
@@ -3,11 +3,24 @@ import {
   defineConfig,
   defineDocs,
   frontmatterSchema,
+  metaSchema,
 } from "fumadocs-mdx/config";
 import { z } from "zod";
 import { remarkImage } from "fumadocs-core/mdx-plugins";
 
-export const { meta, docs } = defineDocs();
+export const { meta, docs } = defineDocs({
+  meta: {
+    schema: metaSchema.extend({
+      description: z.string().optional(),
+    }),
+  },
+  docs: {
+    schema: frontmatterSchema.extend({
+      preview: z.string().optional(),
+      index: z.boolean().default(false),
+    }),
+  },
+});
 
 export const blog = defineCollections({
   type: "doc",
