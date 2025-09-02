965004bf1f6c79e5674b91aa6a0e5deb9f27d3d8
전병길 <jeonbg@kakao.com>
2025-09-02 15:31:02 +0900

    📚 docs: 프론트엔드 마스터 클래스 - 프로그래밍 패러다임 내용 추가
    -  싱글톤 (Singleton)

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,5 +1,6 @@
 import { ErrorCallout, Info, Tip, Warning } from "@/components/mdx/admonition";
 import { LinkButton } from "@/components/mdx/link-button";
+import * as TabsComponents from "fumadocs-ui/components/tabs";
 import { Mermaid } from "@/components/mdx/mermaid";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
@@ -11,6 +12,7 @@ export * from "./link-button";
 export const mdxComponents = {
   Mermaid,
   ...defaultMdxComponents,
+  ...TabsComponents,
   Error: ErrorCallout,
   Info,
   LinkButton,
